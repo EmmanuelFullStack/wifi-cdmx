4b210b1edb97f6f83e07a4f19e1622f333b44ffc
Emmanuel Elias Ramos Altahona <104275764+EmmanuelFullStack@users.noreply.github.com>
2026-03-28 17:04:08 -0500

    feat: add toDomain/fromDomain converters to WifiPointRow

--- src/main/scala/com/arkondata/wificdmx/repository/WifiPointTable.scala
+++ src/main/scala/com/arkondata/wificdmx/repository/WifiPointTable.scala
@@ -1,5 +1,6 @@
 package com.arkondata.wificdmx.repository
 
+import com.arkondata.wificdmx.domain.{Coordinates, WifiPoint}
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.ProvenShape
 
@@ -15,6 +16,16 @@ private[repository] final case class WifiPointRow(
     lat:              Double,
     lon:              Double
 )
+
+private[repository] object WifiPointRow {
+  def toDomain(r: WifiPointRow): WifiPoint =
+    WifiPoint(r.id, r.colonia, r.alcaldia, r.calle, r.programa,
+              r.fechaInstalacion, Coordinates(r.lat, r.lon))
+
+  def fromDomain(p: WifiPoint): WifiPointRow =
+    WifiPointRow(p.id, p.colonia, p.alcaldia, p.calle, p.programa,
+                 p.fechaInstalacion, p.coordinates.lat, p.coordinates.lon)
+}
 
 class WifiPointTable(tag: Tag) extends Table[WifiPointRow](tag, "wifi_points") {
   def id               = column[Long]("id", O.PrimaryKey, O.AutoInc)
