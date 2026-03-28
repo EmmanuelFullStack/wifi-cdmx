219823d1455ba3e7356899aeab0ae09b42b44335
Emmanuel Elias Ramos Altahona <104275764+EmmanuelFullStack@users.noreply.github.com>
2026-03-28 16:49:22 -0500

    feat: add Coordinates value object and fechaInstalacion to WifiPoint

--- src/main/scala/com/arkondata/wificdmx/domain/WifiPoint.scala
+++ src/main/scala/com/arkondata/wificdmx/domain/WifiPoint.scala
@@ -1,9 +1,15 @@
 package com.arkondata.wificdmx.domain
 
+import java.time.LocalDate
+
+final case class Coordinates(lat: Double, lon: Double)
+
 final case class WifiPoint(
-    id:       Long,
-    colonia:  String,
-    alcaldia: String,
-    calle:    String,
-    programa: String
+    id:               Long,
+    colonia:          String,
+    alcaldia:         String,
+    calle:            String,
+    programa:         String,
+    fechaInstalacion: Option[LocalDate],
+    coordinates:      Coordinates
 )
