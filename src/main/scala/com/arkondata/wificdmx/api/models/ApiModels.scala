38adb7505a28d36489ec7b5bcff5aaee3a3761ba
Emmanuel Elias Ramos Altahona <104275764+EmmanuelFullStack@users.noreply.github.com>
2026-03-28 17:16:23 -0500

    feat: add PagedResponse, converters and spray-json formats to ApiModels

--- src/main/scala/com/arkondata/wificdmx/api/models/ApiModels.scala
+++ src/main/scala/com/arkondata/wificdmx/api/models/ApiModels.scala
@@ -1,5 +1,10 @@
 package com.arkondata.wificdmx.api.models
 
+import com.arkondata.wificdmx.domain.{PagedResult, WifiPoint}
+import spray.json._
+
+import java.time.format.DateTimeFormatter
+
 final case class CoordinatesDto(lat: Double, lon: Double)
 
 final case class WifiPointDto(
@@ -12,4 +17,40 @@ final case class WifiPointDto(
     coordinates:      CoordinatesDto
 )
 
+final case class PagedResponse[A](
+    data:       Seq[A],
+    total:      Long,
+    page:       Int,
+    pageSize:   Int,
+    totalPages: Long
+)
+
 final case class ErrorResponse(error: String, message: String)
+
+object WifiPointDto {
+  private val fmt = DateTimeFormatter.ISO_LOCAL_DATE
+
+  def fromDomain(wp: WifiPoint): WifiPointDto =
+    WifiPointDto(
+      id               = wp.id,
+      colonia          = wp.colonia,
+      alcaldia         = wp.alcaldia,
+      calle            = wp.calle,
+      programa         = wp.programa,
+      fechaInstalacion = wp.fechaInstalacion.map(_.format(fmt)),
+      coordinates      = CoordinatesDto(wp.coordinates.lat, wp.coordinates.lon)
+    )
+}
+
+object PagedResponse {
+  def fromDomain[A, B](p: PagedResult[A], f: A => B): PagedResponse[B] =
+    PagedResponse(p.data.map(f), p.total, p.page, p.pageSize, p.totalPages)
+}
+
+object JsonProtocol extends DefaultJsonProtocol {
+  implicit val coordinatesFormat: RootJsonFormat[CoordinatesDto]  = jsonFormat2(CoordinatesDto)
+  implicit val wifiPointFormat:   RootJsonFormat[WifiPointDto]    = jsonFormat7(WifiPointDto.apply)
+  implicit val errorFormat:       RootJsonFormat[ErrorResponse]   = jsonFormat2(ErrorResponse)
+  implicit def pagedFormat[A: JsonFormat]: RootJsonFormat[PagedResponse[A]] =
+    jsonFormat5(PagedResponse.apply[A])
+}
