4ce96a9e71ae740ba5cf6951fe8476f7e8e39058
Emmanuel Elias Ramos Altahona <104275764+EmmanuelFullStack@users.noreply.github.com>
2026-03-28 16:47:54 -0500

    feat: add DatabaseError and ImportError to AppError hierarchy

--- src/main/scala/com/arkondata/wificdmx/domain/AppError.scala
+++ src/main/scala/com/arkondata/wificdmx/domain/AppError.scala
@@ -5,4 +5,6 @@ sealed trait AppError extends Product with Serializable
 object AppError {
   final case class NotFound(message: String) extends AppError
   final case class ValidationError(message: String) extends AppError
+  final case class DatabaseError(message: String, cause: Option[Throwable]) extends AppError
+  final case class ImportError(message: String) extends AppError
 }
